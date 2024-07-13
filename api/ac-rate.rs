d5269672e1bb268ad60e0942b6ee8e3669171b50
emanon001 <emanon001@gmail.com>
2024-07-14 02:40:45 +0900

    feat: vercel-rustを4.0.8にバージョンアップ

--- api/ac-rate.rs
+++ api/ac-rate.rs
@@ -1,66 +1,58 @@
-use http::StatusCode;
 use once_cell::sync::Lazy;
 use serde_json;
 use std::collections::{HashMap, VecDeque};
 use std::convert::TryFrom;
-use std::error::Error;
 use std::sync::Mutex;
 use std::time::{Duration, Instant};
 use url::Url;
 use util::{get_ac_rate, ContestType, ShieldsResponseBody, UserId};
-use vercel_lambda::{error::VercelError, lambda, IntoResponse, Request, Response};
+use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
 
 static ATCODER_REQUEST_TIME_HISTORY: Lazy<Mutex<VecDeque<Instant>>> = Lazy::new(|| {
     let m = VecDeque::new();
     Mutex::new(m)
 });
 
-fn handler(request: Request) -> Result<impl IntoResponse, VercelError> {
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(handler).await
+}
+
+pub async fn handler(request: Request) -> Result<Response<Body>, Error> {
     // get user_id & contest_type from query-string
-    let url = match Url::parse(&request.uri().to_string()) {
-        Ok(url) => url,
-        Err(_) => return Err(VercelError::new("failed parse uri")),
-    };
+    let url = Url::parse(&request.uri().to_string()).map_err(|_| "failed parse uri")?;
     let query_map = url.query_pairs().into_owned().collect::<HashMap<_, _>>();
     let user_id = match query_map
         .get("user_id")
         .and_then(|u| UserId::try_from(u.as_ref()).ok())
     {
         Some(user_id) => user_id,
-        _ => {
-            return Ok(not_found_response(
-                "'user_id' param not found or invalid value".into(),
-            ))
-        }
+        _ => return not_found_response("'user_id' param not found or invalid value".into()),
     };
     let contest_type = match query_map.get("contest_type") {
         Some(contest_type) => match ContestType::try_from(contest_type.as_ref()) {
             Ok(contest_type) => contest_type,
-            Err(_) => return Ok(not_found_response("'contest_type' param is invalid".into())),
+            Err(_) => return not_found_response("'contest_type' param is invalid".into()),
         },
         None => ContestType::Algorithm,
     };
 
     // check rate-limit
     if !check_atcoder_rate_limit() {
-        return Ok(too_many_requests_response(
-            "rate limit has been reached".into(),
-        ));
+        return too_many_requests_response("rate limit has been reached".into());
     }
 
-    let rate = match get_ac_rate(&user_id, contest_type) {
-        Ok(rate) => rate,
-        Err(_) => return Err(VercelError::new("failed get atcoder rate".into())),
-    };
-
+    let rate = get_ac_rate(&user_id, contest_type).map_err(|_| "failed get atcoder rate")?;
     let body = ShieldsResponseBody::new_ac_rate_response(contest_type, rate);
-    let response = Response::builder()
+    Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json; charset=utf-8")
         .header("Cache-Control", "max-age=0, s-maxage=86400")
-        .body(serde_json::to_string(&body).unwrap())
-        .expect("Internal Server Error");
-    Ok(response)
+        .body(
+            serde_json::to_string(&body)
+                .expect("should convert to JSON string")
+                .into(),
+        )?)
 }
 
 fn check_atcoder_rate_limit() -> bool {
@@ -88,23 +80,16 @@ fn check_atcoder_rate_limit() -> bool {
     ok
 }
 
-fn not_found_response(mes: String) -> Response<String> {
-    Response::builder()
+fn not_found_response(mes: String) -> Result<Response<Body>, Error> {
+    Ok(Response::builder()
         .status(StatusCode::NOT_FOUND)
         .header("Content-Type", "text/plain")
-        .body(mes)
-        .unwrap()
+        .body(mes.into())?)
 }
 
-fn too_many_requests_response(mes: String) -> Response<String> {
-    Response::builder()
+fn too_many_requests_response(mes: String) -> Result<Response<Body>, Error> {
+    Ok(Response::builder()
         .status(StatusCode::TOO_MANY_REQUESTS)
         .header("Content-Type", "text/plain")
-        .body(mes)
-        .unwrap()
-}
-
-// Start the runtime with the handler
-fn main() -> Result<(), Box<dyn Error>> {
-    Ok(lambda!(handler))
+        .body(mes.into())?)
 }
